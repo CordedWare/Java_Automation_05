a9eb9db1d51429a24b6dba9589ddd0611b8bc264
daev93@mail.ru <daev93@mail.com>
2022-10-16 22:41:51 +0500

    new commit (add new class FizzBuzz & FizzBuzzTest)v2 16.10.22

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,81 @@
-package PACKAGE_NAME;public class FizzBuzzTest {
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+
+public class FizzBuzzTest {
+
+    // 1. Positive testing
+    // if (start <= end)
+    // return array;
+
+    // start < end
+    @Test
+    public void testStartLessThanEnd_HappyPathTest() {
+        // AAA
+        // Arrange
+        int start = 1;
+        int end = 20;
+        String[] exceptecResult = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
+                "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz"};
+
+        // Act
+        String[] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        // Assert
+        Assert.assertEquals(exceptecResult, actualResult);
+    }
+
+
+    // start < end
+    @Test
+    public void testStartEqualsEnd_HappyPathTest() {
+        // AAA
+        // Arrange
+        int start = 1;
+        int end = 1;
+        String[] exceptecResult = {"1"};
+
+        // Act
+        String[] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        // Assert
+        Assert.assertEquals(exceptecResult, actualResult);
+    }
+
+
+    // start < end
+    @Ignore
+    @Test
+    public void testStartLessThanEnd_StartEndAreNegative_HappyPathTest() {
+        // AAA
+        // Arrange
+        int start = -20;
+        int end = -1;
+        String[] exceptecResult = {"Buzz"};
+
+        // Act
+        String[] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        // Assert
+        Assert.assertEquals(exceptecResult, actualResult);
+    }
+
+    // 2. Negative testing
+    // if (start > end)
+    // return new String[0];
+
+    @Test
+    public void testStartGreaterThanEnd_NegativeTest() {
+        // AAA
+        // Arrange
+        int start = 20;
+        int end = 1;
+        String[] exceptecResult = {};
+
+        // Act
+        String[] actualResult = new FizzBuzz().fizzBuzz(start, end);
+
+        // Assert
+        Assert.assertEquals(exceptecResult, actualResult);
+    }
 }
